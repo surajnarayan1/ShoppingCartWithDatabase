0e65c7eaa5a6ef3af2a67eca77f84b813a677631
Suraj Narayan <snarayan@tavisca.com>
2017-09-18 21:39:05 +0530

    rectified asked changes

---
+++ shopping Cart/EditProduct.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Data;
+
+namespace shopping_Cart
+{
+    public partial class EditProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var pname = TextBox1.Text;
+            var quantity = TextBox2.Text;
+            var price = Convert.ToInt32(TextBox3.Text);
+            try
+            {
+
+                using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["SampleShoppingCartConnectionString"].ConnectionString))
+                {
+                    string pId = Request.QueryString["productId"];
+                    SqlCommand cmd = new SqlCommand($"UPDATE Products SET Pname = '{pname}', Quantity= {quantity}, Price= {price} where Pid = '" + pId + "'");
+                    cmd.Connection = connection;
+                    //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                    DataSet dataset = new DataSet();
+                    dataAdapter.Fill(dataset);
+
+
+                    Response.Redirect("ProductManipulation.aspx");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Exception  arised while updating  new item" + ex.Message + "')</script>");
+
+            }
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("AddItem.aspx");
+        }
+    }
+}
---
+++ shopping Cart/InventoryItem.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace shopping_Cart
+{
+    public partial class InventoryItem : System.Web.UI.Page
+    {
+        Dictionary<string, float> cartItem;
+        // SqlConnection connect;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (!IsPostBack)
+                {
+                    using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["SampleShoppingCartConnectionString"].ConnectionString))
+                    {
+                        // connect = new SqlConnection(@"Data Source=TAVDESK055;Initial Catalog=SampleShoppingCart;User Id=sa;Password=test123!@#");
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("select * from Products", connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        GridView1.DataSource = reader;
+                        GridView1.DataBind();
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Exception arised whenever the Connection Establised during pageload" + ex.Message + "')</script>");
+
+            }
+
+        }
+
+        protected void Gridview_row_command(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName == "Add")
+                {
+
+                    int rowIndex = Int32.Parse((string)e.CommandArgument);
+                    GridViewRow row = GridView1.Rows[rowIndex];
+                    var pName = row.Cells[0].Text;
+                    var cost = row.Cells[3].Text;
+                    if (ViewState["cartItem"] == null)
+                    {
+                        cartItem = new Dictionary<string, float>();
+                        cartItem[pName] = Convert.ToInt32(cost);
+                        ViewState["cartItem"] = cartItem;
+                        HttpContext.Current.Session["cartItem"] = cartItem;
+                    }
+                    else
+                    {
+                        {
+                            cartItem = new Dictionary<string, float>();
+                            cartItem = (Dictionary<string, float>)ViewState["cartItem"];
+                            cartItem[pName] = int.Parse(cost);
+                            HttpContext.Current.Session["cartItem"] = cartItem;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Exception  arised while loading the table" + ex.Message + "')</script>");
+
+            }
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+                Response.Redirect("Cart.aspx");
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ProductManipulation.aspx");
+        }
+    }
+}
